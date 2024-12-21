ef39b62d2855ea98cebd6c5061251ed71822a526
dblok0x <dblok0x@gmail.com>
2024-12-21 15:09:05 +0100

    ee

--- app/page.js
+++ app/page.js
@@ -1,8 +1,25 @@
 import Image from "next/image";
 import logo from "./logo.png"
+import logo1 from "./logo1.png"
+import logo2 from "./logo2.png"
+import gal from "./gal.jpg"
+import gal1 from "./gal1.jpg"
+import gal2 from "./gal2.jpg"
+import gal3 from "./gal3.jpg"
+import gal4 from "./gal4.jpg"
+import gal15 from "./gal15.jpg"
+import gal16 from "./gal16.jpg"
+import gal5 from "./gal5.jpg"
+import gal6 from "./gal6.jpg"
+import gal7 from "./gal7.jpg"
+import gal8 from "./gal8.jpg"
+import gal9 from "./gal9.jpg"
+import gal10 from "./gal10.jpg"
+import gal11 from "./gal11.jpg"
 import enter from "./enter.png"
 import pear from "./pear.gif"
 import pear1 from "./pear1.gif"
+import pear2 from "./pear2.gif"
 export default function Home() {
   return (
     <div
@@ -27,35 +44,138 @@ export default function Home() {
               <span class="self-stretch my-auto">Enter</span>
             </a></div>
     <div
-      class="flex flex-col justify-center items-center px-96 py-8 max-w-full min-h-[900px] w-[1280px] max-md:px-5 max-md:py-24"
+      class="flex flex-col justify-center items-center py-8 max-w-full min-h-[900px] w-[1280px] max-md:px-5 max-md:py-24"
     >
-      <div class="flex flex-col items-center w-full">
-        <div class="flex flex-col w-64 max-w-full min-h-[256px]">
+      <div class=" flex    flex-col items-center w-full">
+        <div class="flex mx-96  flex-col w-64 max-w-full min-h-[256px] border-4 border-black">
           <Image
-            src={logo}
+            src={logo2}
             alt="Pear Logo"
             class="object-contain w-full rounded-2xl aspect-square"
-            width={256}
-            height={256}
+            width={266}
+            height={266}
           />
-          
+        
+         
         </div>
-    
-        <Image
+        <p className="text-zinc-100 pt-2 text-sm font-thin font-mono "> (Create, Deploy, Buy, Finalize)</p>
+  <h1 className="text-center text-green-200 text-2xl font-semibold pt-16 ">About</h1>
+   <p className="text-center text-zinc-800 pt-2 px-16 md:px-20 lg:px-56">
+PEAR is pioneering the future of AI trading on Hyperliquid as the first dedicated AI agent launchpad. Our innovative platform creates a secure environment where AI trading agents can be deployed while protecting genuine traders through our advanced anti-bot and anti-sniper mechanisms.
+</p>
+<div className=" max-w-full min-h-[256px] "><Image
             src={pear1}
             alt="Pear Logo"
-            class="object-contain w-full rounded-2xl aspect-square"
+            class="object-contain rounded-2xl aspect-square"
             width={256}
-            height={156}
-          />
-        <p
-          class="mt-8 text-2xl font-medium leading-none text-center text-green-200"
+            height={136}
+          /></div>
+        
+
+  <div className="pt-16 lg:pt-28">
+  <h1 className="text-center text-green-100 text-2xl font-semibold ">Gallery</h1>
+    <div class="grid grid-cols- pt-4 gap-4 md:grid-cols-3">
+  <div class="grid gap-4">
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center"
+        src={gal}
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center "
+        src={gal11}
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center"
+        src={gal15}
+        alt="gallery-photo"
+      />
+    </div>
+    
+  </div>
+  <div class="grid gap-4">
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center"
+        src={gal3}
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center"
+        src={gal4}
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center "
+        src={gal5}
+        alt="gallery-photo"
+      />
+    </div>
+  </div>
+  <div class="grid gap-4">
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center"
+        src={gal16}
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center "
+        src={gal7}
+        alt="gallery-photo"
+      />
+    </div>
+    <div>
+      <Image
+      width={86}
+      height={86}
+        class="h-auto max-w-full rounded-lg object-cover object-center"
+        src={gal8}
+      />
+    </div>
+  </div>
+  <div class="grid gap-4">
+  
+  </div>
+</div></div>
+
+
+        <div
+          class="flex flex-col items-center justify-center pt-8 mt-8 text-sm font-medium text-center"
+        >     <p
+          class="mt-4 px-20 md:px-48 xl:px-96 text-2xl font-medium leading-none text-center text-green-200"
         >
           (Powerful Engine AI Robots) built on top of High PEAR Liquid
         </p>
-        <div
-          class="flex flex-col items-center justify-center pt-8 mt-8 text-sm font-medium text-center"
-        >
+          <div className="w-full pt-4 "></div>
           <div class="flex gap-4 items-center">
             <a
               href="https://app.hyperliquid.xyz/trade/0x1ad748b576a84f6eb6541389bb53d164"
@@ -164,4 +284,61 @@ export default function Home() {
         />
       </div>
     </nav>
-  </div>*/
+  </div>*/ 
+  
+  
+  
+  
+  
+  
+  //galery
+  /*
+  <div>
+
+<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
+        </div>
+    </div>
+</div>
+</div>*/
